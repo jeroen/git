7d74f80aaacf78753e390eeaaa810cf5bd4f57aa
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-02 15:57:01 -0800

    Update libgit2

--- tools/libgit2.rb
+++ tools/libgit2.rb
@@ -2,9 +2,10 @@ require "formula"
 
 class Libgit2 < Formula
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v0.21.3.tar.gz"
-  sha1 "d116cb15f76edf2283c85da40389e4fecc8d5aeb"
+  url "https://github.com/libgit2/libgit2/archive/v0.22.1.tar.gz"
+  sha1 "20223ca392bb8f5d891cd9944eec62c6a6e21dd5"
 
+  keg_only "dont link global libs"
   head "https://github.com/libgit2/libgit2.git"
 
   depends_on "cmake" => :build
@@ -15,6 +16,7 @@ class Libgit2 < Formula
     args = std_cmake_args
     args << "-DBUILD_TESTS=NO"
     args << "-DBUILD_SHARED_LIBS=OFF"
+    args << "-DBUILD_CLAR=OFF"
 
     mkdir "build" do
       system "cmake", "..", *args
